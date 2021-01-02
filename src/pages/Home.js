98abdb26eb4b624c75bca5960beb65d1e964b7f4
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2021-01-02 20:48:57 +0530

    added search functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import './Home.css'
 import { Link } from "react-router-dom";
 import AppsIcon from "@material-ui/icons/Apps"
 import { Avatar } from '@material-ui/core';
+import Search from '../components/Search';
 
 function Home() {
     return (
@@ -17,16 +18,23 @@ function Home() {
                     {/* icon */}
                     <AppsIcon className="home__appsicon" />
                     {/* avatar */}
-                    <Avatar />
+                    <Avatar >AR</Avatar>
                 </div>
             </div>
             <div className="home__body">
+
+                <img
+                    src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+                    alt="Google"
+                />
+                <div className="home__inputContainer">
+                    <Search />
+                </div>
 
             </div>
             <div className="home__footer">
 
             </div>
-            {/* <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" /> */}
         </div>
     )
 }
