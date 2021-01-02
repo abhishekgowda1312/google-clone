229fadb53371b19ccd45b0338d2aa21148de6210
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2021-01-02 18:49:33 +0530

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div>
+            {/* <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" /> */}
+        </div>
+    )
+}
+
+export default Home
